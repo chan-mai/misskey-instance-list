156baaf9b60f76c11c4e623f5643041c89697ff6
K Nishi <94899581+nw3q@users.noreply.github.com>
2025-12-30 00:21:21 +0900

    fix: Default Style

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -1,5 +1,5 @@
 export const PAGE_SIZE = 30;
-export const STORAGE_KEY = 'miHub_server_finder';
+export const STORAGE_KEY = 'miHub_server_finder_v2';
 
 export const SORT_API_MAP: Record<SortField, string> = {
   recommendedScore: 'recommended',
